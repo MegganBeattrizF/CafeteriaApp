ab640a63e8ec1cd9e862d9e20224339593f196fd
Ana Beatriz Lisboa <megganbeattrizfrutuoso@gmail.com>
2020-10-22 15:34:19 -0300

    * Criação do layout activity_sign_up.xml e ajuste do layout activity_apresentation.xml para fullscreen.

--- app/src/main/java/com/android/cafeteriaapp/LoginActivity.java
+++ app/src/main/java/com/android/cafeteriaapp/LoginActivity.java
@@ -2,13 +2,45 @@ package com.android.cafeteriaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btnSignIn, btnSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initializeComponents();
+        callbacksToActivity();
+
+    }
+
+    private void initializeComponents() {
+        btnSignIn = findViewById(R.id.button_sign_in_confirm);
+        btnSignUp = findViewById(R.id.button_sign_up_actv);
+    }
+
+    private void callbacksToActivity(){
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),SignUpActivity.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/android/cafeteriaapp/SignUpActivity.java
+++ app/src/main/java/com/android/cafeteriaapp/SignUpActivity.java
@@ -2,13 +2,44 @@ package com.android.cafeteriaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    Button btnSignIn, btnSignUpConfirmation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        initializeComponents();
+        callbacksToActivity();
+    }
+
+    private void initializeComponents() {
+        btnSignIn = findViewById(R.id.button_sign_in_login_actv);
+        btnSignUpConfirmation = findViewById(R.id.button_sign_up_confirm);
+    }
+
+    private void callbacksToActivity(){
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                finish();
+            }
+        });
+
+        btnSignUpConfirmation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        });
     }
 }
